ae01d4f46f2974a6e68687c9afc4cc5bdc04b84e
JorgeOteiza <oteiza.jor@gmail.com>
2026-03-27 23:57:56 -0300

    chore: configuración inicial del proyecto con backend en Flask y frontend en Vite + React + TypeScript

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from extensions import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    @app.route("/")
+    def home():
+        return {"message": "API running"}
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
